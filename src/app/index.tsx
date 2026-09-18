c038709395c016d01ddbba94090dc8770a7a49c2
Edwin-guo <edwin.guo@edu.sait.ca>
2026-09-17 21:48:30 -0600

    Header completed

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,31 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+const onPressFunction = () => alert("Alert Button Pressed");
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <View style={styles.header}>
+        <View style={styles.side}>
+          <Image
+            source={require("../../assets/images/left-arrow.png")}
+            style={{ width: 40, height: 40 }}
+          />
+        </View>
+        <View style={styles.center}>
+          <Text style={[styles.faded, styles.bold]}>OOTD_EVERDAY</Text>
+          <Text style={[styles.bold, { fontSize: 20 }]}>Posts</Text>
+        </View>
+        <View style={styles.side}></View>
+      </View>
+      <View style={styles.border}></View>
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+
+        <Pressable onPress={onPressFunction}>
+          <Text>Alert</Text>
+        </Pressable>
+      </View>
+    </>
   );
 }
 
@@ -13,5 +34,31 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  faded: {
+    color: "#a6a6a6",
+  },
+  bold: {
+    fontWeight: "bold",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    marginTop: 50,
+    marginBottom: 10,
+    paddingHorizontal: 30,
+  },
+  center: {
+    alignItems: "center",
+    flex: 2,
+  },
+  side: {
+    flex: 1,
+  },
+  border: {
+    borderWidth: 1,
+    borderColor: "#a6a6a6",
   },
 });
